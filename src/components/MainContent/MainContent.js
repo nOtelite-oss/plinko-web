e1701be8cd14f663f7a9aa6c55b72016a98cd66f
Emir Topaloglu <nonotelite@gmail.com>
2023-04-25 20:53:23 +0300

    feat: simulation value form added

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -3,14 +3,38 @@ import classes from './MainContent.module.css';
 const MainContent = () => {
   return (
     <div>
+      <h1 className={classes.MainHeader}>Plinko Simulation</h1>
       <form className={classes.MainContainer}>
         <div className={classes.FormContent}>
-          <label for='box-input'>Box Number:</label>
-          <input type='number' name='box-input' placeholder='Ex: 5' />
+          <label className={classes.FormLabel} htmlFor='box-input'>
+            Box Number:
+          </label>
+          <input
+            className={classes.FormInput}
+            type='number'
+            id='box-input'
+            name='box-input'
+            placeholder='Ex: 5'
+            min={1}
+            max={29}
+          />
         </div>
         <div className={classes.FormContent}>
-          <label for='ball-input'>Ball Input:</label>
-          <input type='number' name='ball-input' placeholder='Ex: 1000000' />
+          <label className={classes.FormLabel} htmlFor='ball-input'>
+            Ball Input:
+          </label>
+          <input
+            className={classes.FormInput}
+            type='number'
+            id='ball-input'
+            name='ball-input'
+            placeholder='Ex: 1000000'
+          />
+        </div>
+        <div className={classes.FormContent}>
+          <button className={classes.SubmitButton} type='submit'>
+            Simulate
+          </button>
         </div>
       </form>
     </div>
